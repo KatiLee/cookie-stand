804d13ec090d9b2a16fb295b65d12a84b8225b2b
Katherine <katherineleefoley@gmail.com>
2022-11-10 20:50:59 -0600

    adding some arrays in a fashion i am hoping will be helpful when it comes to adding in the random generator

---
+++ app.js
@@ -0,0 +1,20 @@
+let seattle = {
+  minMaxArray: ['23', '65', '6.3']
+};
+
+let tokyo = {
+  minMaxArray: ['3', '24', '1.2']
+
+};
+
+let dubai = {
+  minMaxArray: ['11', '38', '3.7']
+};
+
+let paris = {
+  minMaxArray: ['20', '38', '2.3']
+};
+
+let lima = {
+  minMaxArray: ['2', '16', '4.6']
+};
