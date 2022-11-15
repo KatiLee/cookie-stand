e99f21f63dafc3d6694ecffe31259ce55cb4296b
tektechnologies <info@tektechnologies.com>
2022-11-14 18:41:53 -0600

    build out js start

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@
 console.log('js file connected.');
 
 
-
-
 let seattle = {
-  minMaxArray: ['23', '65', '6.3']
+  minCust : 23,
+  maxCust : 65,
+  avgCookieSale: 6.3,
+  hourlyArrayCookies : [],
+  numOfCustomerPerHour: function(){
+    console.log('inside the num of customer function.');
+  },
+  cookiesPerHour: function(){
+    console.log('inside the cookies per hour');
+  }
 };
+seattle.numOfCustomerPerHour();
+seattle.cookiesPerHour();
+
+
 
 let tokyo = {
   minMaxArray: ['3', '24', '1.2']
@@ -24,3 +35,29 @@ let paris = {
 let lima = {
   minMaxArray: ['2', '16', '4.6']
 };
+
+
+
+console.log('stores: ', seattle, tokyo, dubai, paris, lima);
+
+
+
+// Sales Data
+
+function displayData(location, id){
+  // Display the values of each array as unordered lists in the browser.
+  let parentElement = document.getElementById(id);
+  console.log(parentElement, location);
+  // continue to loop the data and create an li and append that info to the parentElements.
+
+
+
+
+}
+
+
+
+
+
+displayData(seattle, 'seattle');
+//add the other objects
